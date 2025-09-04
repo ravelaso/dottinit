0ae6e669c7bc38590d53bb7485b096ffba56b6e7
Andres Ravelo <ravelaso@proton.me>
2025-09-04 19:29:16 +0200

    Update setup.sh

--- zsh/setup.sh
+++ zsh/setup.sh
@@ -4,4 +4,5 @@ set -e
 echo "Configuring Zsh..."
 chsh -s "$(command -v zsh)"
 cp "$SCRIPT_DIR/zsh/config/zshrc" ~/.zshrc
+sudo cp "$SCRIPT_DIR/zsh/config/zshrc" /root/.zshrc
 echo "Zsh configuration completed!"
